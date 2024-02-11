0da55aaaa963b3ea6e51a8c710f5843f22cd0c36
Munna Ahmed <munnaahmed2025@gmail.com>
2024-02-11 18:23:14 +0600

    commit

--- dynamicImport.js
+++ dynamicImport.js
@@ -2,14 +2,14 @@ console.log("code is live..");
 
 // this code only works with dynamic import
 if (true) {
-    const combine = await import("./modules/combine.js");
+    const combine = await import("./esmodule/node/modules/combine.js");
     console.log(combine);
     console.log(combine.sayMyAge(3));
 }
 
 // console.log(await import("./modules/combine.js"));
 
-const combine = import("./modules/combine.js").then((result) =>
+const combine = import("./esmodule/node/modules/combine.js").then((result) =>
     console.log(result)
 );
 
@@ -18,8 +18,8 @@ const combine = import("./modules/combine.js").then((result) =>
 // like this too - import multiple module with promise api
 
 const promises = Promise.all([
-    await import("./modules/calc.js"),
-    await import("./modules/whoami.js"),
+    await import("./esmodule/node/modules/calc.js"),
+    await import("./esmodule/node/modules/whoami.js"),
 ]);
 
 // console.log(promises);
