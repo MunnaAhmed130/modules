22958006968ee7174137703ae81a920f73791859
Munna Ahmed <munnaahmed2025@gmail.com>
2024-02-11 06:00:41 +0600

    commit

---
+++ esmodule/dynamicImport.js
@@ -0,0 +1,20 @@
+console.log("code is live..");
+
+// this code only works with dynamic import
+if (true) {
+    const combine = await import("./modules/combine.js");
+    console.log(combine.sayMyAge(3));
+}
+
+// like this too
+const promises = Promise.all([
+    await import("./modules/calc.js"),
+    await import("./modules/whoami.js"),
+]);
+
+// console.log(promises);
+
+promises.then((result) => {
+    console.log(result);
+    console.log(result[0].sum(4, 5));
+});
